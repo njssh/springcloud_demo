58c26b423cf8480e2343a5d6bde668ed27071eff
niujs <niujs@yonyou.com>
2017-07-11 13:57:05 +0800

    配置中心服务化

--- spring-cloud/config-client/src/main/java/com/njs/demo/client/Application.java
+++ spring-cloud/config-client/src/main/java/com/njs/demo/client/Application.java
@@ -2,8 +2,11 @@ package com.njs.demo.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//增加@EnableDiscoveryClient注解，用来发现config-server服务，利用其来加载应用配置
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
--- spring-cloud/config-server/src/main/java/com/njs/demo/config/Application.java
+++ spring-cloud/config-server/src/main/java/com/njs/demo/config/Application.java
@@ -2,8 +2,11 @@ package com.njs.demo.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
+//新增@EnableDiscoveryClient注解，用来将config-server注册到上面配置的服务注册中心上去
 @EnableConfigServer
 //添加@EnableConfigServer注解，开启Config Server
 @SpringBootApplication
